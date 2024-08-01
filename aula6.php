b143768c1c72506670799dfc970ea470ee7126c9
ℒ𝒶𝒾𝓈 <169085542+laiscarvalhox@users.noreply.github.com>
2024-08-01 14:58:35 -0300

    Add files via upload

---
+++ aula6.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: pink;
+        }
+    </style>
+</head>
+<body>
+    
+    <?php
+
+    echo "<h1>Arrays - Vetores</h1>"; #quando trabalhamos com  arrays começa na posição 0, como ocorre no caso do fiesta e palio
+    $carros = array('Fiesta', 'Palio', 'Corsa', 'Siena');
+    echo $carros [1], "<br>";
+    echo $carros[3], "<br>";
+
+    ?>
+
+</body>
+</html>
